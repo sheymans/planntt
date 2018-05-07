a5637ca26feca453e8b381ca703ce9701468a522
Stijn Heymans <stijn.heymans@gmail.com>
2018-05-07 10:54:43 -0700

    Initial Commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'planntt'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
