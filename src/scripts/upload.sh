43cc67e6cd1d3a255f58ccc7a62399e29cd5e802
Stijn Heymans <stijn.heymans@gmail.com>
2018-05-18 11:30:34 -0700

    Not building linux versions
    One gets shared libraries errors etc.

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -5,7 +5,6 @@ version=$1
 echo "copying apps with version ${version}.."
 scp ~/Workspaces/planntt/build/planntt-${version}.dmg \
     ~/Workspaces/planntt/build/planntt\ Setup\ ${version}.exe \
-    ~/Workspaces/planntt/build/planntt-${version}-x86_64.AppImage \
     ~/Workspaces/planntt/build/*.yml \
     ~/Workspaces/planntt/build/*.yaml \
     iwokeupthismorning.net@s88322.gridserver.com:~/domains/planntt.com/html/downloads/
