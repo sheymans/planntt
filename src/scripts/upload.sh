1438d681ee0b3588dffb4641e574469799cd2d2d
Stijn Heymans <stijn.heymans@gmail.com>
2018-05-31 10:24:29 -0700

    Linux version

--- src/scripts/upload.sh
+++ src/scripts/upload.sh
@@ -6,6 +6,7 @@ echo "copying apps with version ${version}.."
 scp ~/Workspaces/planntt/build/planntt-${version}.dmg \
     ~/Workspaces/planntt/build/planntt-${version}-mac.zip \
     ~/Workspaces/planntt/build/planntt\ Setup\ ${version}.exe \
+    ~/Workspaces/planntt/build/planntt-${version}-x86_64.AppImage \
     ~/Workspaces/planntt/build/*.yml \
     ~/Workspaces/planntt/build/*.yaml \
     iwokeupthismorning.net@s88322.gridserver.com:~/domains/planntt.com/html/downloads/
