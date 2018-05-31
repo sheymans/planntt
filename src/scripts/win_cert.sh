15fe9edaf9f23722be0c11f51f68267675e9f56f
Stijn Heymans <stijn.heymans@gmail.com>
2018-05-31 09:54:46 -0700

    Enable 64 and 32 bit build for windows

--- src/scripts/win_cert.sh
+++ src/scripts/win_cert.sh
@@ -4,4 +4,4 @@ export WIN_CSC_LINK=file:///project/keys/windows_digicert.p12
 read -sp "Certificate Password: " certPassword
 export WIN_CSC_KEY_PASSWORD=$certPassword;
 echo "Cert location: " $WIN_CSC_LINK
-node .electron-vue/build.js && electron-builder -w
+node .electron-vue/build.js && electron-builder -w --x64 --ia32
