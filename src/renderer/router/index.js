1a984965b5b9cce667861046e63503cc55becfd7
Stijn Heymans <stijn.heymans@gmail.com>
2021-08-11 13:26:52 -0700

    Remove initial LandingPage

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,11 +6,6 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/landing-page',
-      name: 'landing-page',
-      component: require('@/components/LandingPage').default
-    },
-    {
       path: '/',
       name: 'main-page',
       component: require('@/components/MainPage').default
